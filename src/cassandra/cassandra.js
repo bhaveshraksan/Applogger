20c5e4abce5ddc902f9d105a96879852fb46f4a6
Shubhankit Saxena <shubhankit.saxena@raksan.in>
2017-12-20 17:21:13 +0530

    adds the cassandra schema for applog

--- src/cassandra/cassandra.js
+++ src/cassandra/cassandra.js
@@ -1,7 +1,7 @@
 var cassandra = require('cassandra-driver')
 
 var client = new cassandra.Client({
-    contactPoints: ['***.***.***.***:9042']
+    contactPoints: ['13.59.46.187:9042'] // Insert the dev IP here
 });
 
 function createKeyspace() {
@@ -18,13 +18,22 @@ function createTypes() {
 }
 function createTableInCassandra() {
     var query ="CREATE TABLE IF NOT EXISTS logs.auditLogs" +
-        "(id text,TimeStamp text,queryName text,taskName text,userId text PRIMARY KEY,newValue text,oldValue text,moduleName text,newValue2 frozen <newValueType>,audit text,);"
+        "(userId text,userName text,collectionName text,url text,docId text PRIMARY KEY,action text,field text,previousValue text,currentValue text,timeStamp text);"
     return client.execute(query);
 
 }
 
+function insertData(newObject){
+    var insertRepo = 'INSERT INTO logs.auditLogs (userId,userName,collectionName,url,docId,action,field,previousValue,currentValue,timeStamp) '
+    + 'VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
 
-function createSchemaCassandra() {
+    return client.execute(insertRepo,
+                   [newObject.userId,newObject.userName,newObject.collectionName,newObject.url,newObject.docId,newObject.action,newObject.field,newObject.previousValue,newObject.currentValue,newObject.timeStamp],
+                   { prepare : true });
+}
+
+
+function createSchemaCassandra(newObject) {
     client.connect()
         .then(function () {
             return createKeyspace()
@@ -34,6 +43,9 @@ function createSchemaCassandra() {
         })
         .then(function () {
             return createTableInCassandra()
+        })
+        .then(function () {
+            return insertData(newObject)
         })
         .then(function () {
             client.shutdown();
@@ -46,4 +58,18 @@ function createSchemaCassandra() {
 
 
 
-createSchemaCassandra()
+exports.createSchemaCassandra = createSchemaCassandra; 
+// var obj = {"userId":"nrT5d5B8ct8pLyZSi",
+// "userName":"platformadmin@moolya.global",
+// "collectionName":"MlSubChapters",
+// "url":"http://localhost:3000/admin/chapters/hwzPbwq5saa6LuhHD/DT9Y8iQEKuGa3f7R6/QvpiBt9HCGoqFGri5/Moolya-Ahmedabad/subChapterDetails",
+// "docId":"QvpiBt9HCGoqFGri5",
+// "action":"E",
+// "field":"subChapterDisplayName",
+// "previousValue":"\"Moolyad\"",
+// "currentValue":"\"Moolyaddd\"",
+
+// "userAgent":{"OS":"-","ipAddress":"127.0.0.1","browser":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36","deviceModel":"-","deviceType":"-","deviceVendor":"-"},
+
+// "timeStamp":"2017-12-20T10:56:00.247Z","clusterId":"all","chapterId":"all","subChapterId":"all","communityId":"all","communityCode":"all","clusterName":"all","chapterName":"all","subChapterName":"all","moduleName":"SUBCHAPTER","fieldName":"subChapterDisplayName"};
+// createSchemaCassandra(obj);
