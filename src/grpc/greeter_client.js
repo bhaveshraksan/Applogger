ef395d38e90ac009a2270c8552743a28ef7b8507
Kanwarjeet Singh <kanwarjeet.singh@raksan.in>
2018-01-05 15:49:00 +0530

    Audit: Cassandra Insertion, Working: Fetching Cassandra and GRPC

--- src/grpc/greeter_client.js
+++ src/grpc/greeter_client.js
@@ -3,29 +3,16 @@ console.log(PROTO_PATH);
 var grpc = require('grpc');
 var audits_proto = grpc.load(PROTO_PATH).getAuditsLogsPkg;
 
- function checkPromise() {
-
-    return  new Promise((resolve, reject) => {
-
-        resolve({kk:1});
-
-    })
-}
-function main() {
+async function main() {
     var client = new audits_proto.AuditsLogs('localhost:50051',
         grpc.credentials.createInsecure());
     var requestObj = {
         skip:1,
         limit:10
     }
-    return checkPromise().then((x)=>{console.log(x)});
     client.getAuditLogs(requestObj,function (err,res) {
-        console.log(2);
+        console.log(res);
     });
-
-
-
-    console.log(1)
 }
 
 main();
